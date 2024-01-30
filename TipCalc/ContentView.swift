2f43bad21996007d8481b6de57f264625f36e44f
kohei machida <kmachida12345@gmail.com>
2024-01-30 18:31:36 +0900

    Create project

---
+++ TipCalc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipCalc
+//
+//  Created by 町田康平 on 2024/01/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
