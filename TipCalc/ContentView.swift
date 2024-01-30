80a96fa7adf0045e650dbc7f1c938eb1ec7785a1
kohei machida <kmachida12345@gmail.com>
2024-01-30 21:15:25 +0900

    feature: add the tip calculation screen

--- TipCalc/ContentView.swift
+++ TipCalc/ContentView.swift
@@ -8,15 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = ""
+    @State private var tip = 0.0
+    @State private var showAlert = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text("請求額")) {
+                    TextField("金額を入力", text: $amount)
+                        .keyboardType(.decimalPad)
+                }
+
+                Section(header: Text("チップ")) {
+                    Text("\(calculateTip(), specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("チップ計算")
+            .alert(isPresented: $showAlert) {
+                            Alert(
+                                title: Text("エラー"),
+                                message: Text("無効な入力です"),
+                                dismissButton: .default(Text("OK")) {
+                                    self.amount = ""
+                                    self.showAlert = false
+                                }
+                            )
+                        }
         }
-        .padding()
     }
+
+    func calculateTip() -> Double {
+        let tipPercentage = 0.10
+
+        if amount.isEmpty {
+            return 0.0
+        }
+
+        guard let amountValue = Double(amount) else {
+            DispatchQueue.main.async {
+                self.showAlert = true
+            }
+            return 0.0
+        }
+        return amountValue * tipPercentage
+     }
 }
 
 #Preview {
